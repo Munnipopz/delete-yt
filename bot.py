c69ecd052fb25387edfcab24acdfa1328f368d83
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-10-03 22:39:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import subprocess
 
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, RegexHandler, MessageHandler, Filters
-from telegram.ext import Update
+from telegram.ext import Updater
 
 from sample_config import Config
 
