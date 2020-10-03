311b613fd95f3b472fa4ccec0ac64ee55b54def4
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-10-03 22:59:16 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,4 +4,4 @@ class Config(object):
     # get a token from @BotFather  
     TG_BOT_TOKEN = os.environ.get("1233265878:AAHJDgJgdskG-tbctKlElKZfXmEWdsOiuOg")
     # Your Telegram Id
-    OWNER_ID = int(os.environ.get('984441749')
+    OWNER_ID = os.environ.get('984441749')
